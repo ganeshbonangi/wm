b1c117bfd5a19470bfb1890c12b648920f59a771
unknown <Ganesh.Bonangi@LT0028169.in.ril.com>
2015-10-25 11:06:39 +0530

    renamed usercreation tpl path

--- client/components/usercreation/user.js
+++ client/components/usercreation/user.js
@@ -146,7 +146,7 @@ angular.module('workerManagementSystemApp')
     // require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
     // restrict: 'A', // E = Element, A = Attribute, C = Class, M = Comment
     // template: '',
-     templateUrl: '/components/userCreation/user.html',
+     templateUrl: '/components/usercreation/user.html',
      replace: true,
     // transclude: true,
     // compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
